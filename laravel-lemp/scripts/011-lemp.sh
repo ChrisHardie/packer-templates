b203c7d71b5e224e8fbdcad246b5c0067216170c
Chris Hardie <chris@chrishardie.com>
2022-04-10 12:19:26 -0400

    Initial commit of a working packer setup

---
+++ laravel-lemp/scripts/011-lemp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rvf /etc/nginx/sites-enabled/default
+
+ln -s /etc/nginx/sites-available/digitalocean \
+      /etc/nginx/sites-enabled/digitalocean
+
+rm -rf /var/www/html/index*debian.html
+
+chown -R www-data: /var/www
+
+sed -i 's/supervised no/supervised systemd/g' /etc/redis/redis.conf
